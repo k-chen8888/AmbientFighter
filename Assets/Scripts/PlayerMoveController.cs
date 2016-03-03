3454fa2626ea99ea9b5494f06c495c97e63ed55e
k-chen8880 <k.chen8880@gmail.com>
2016-03-03 17:25:24 -0500

    Added networking script from tutorial
    http://docs.unity3d.com/Manual/UNetSetup.html

---
+++ Assets/Scripts/PlayerMoveController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerMoveController : NetworkBehaviour
+{
+	public Vector3 startPosition;
+
+	// Use this for initialization
+	void Start () {
+		transform.Translate(startPosition);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isLocalPlayer)
+			return;
+		
+		var x = Input.GetAxis("Horizontal")*0.1f;
+		var z = Input.GetAxis("Vertical")*0.1f;
+
+		transform.Translate(x, 0, z);
+	}
+}
