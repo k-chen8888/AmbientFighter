8da0aa6a3776c5b44533d7a9f0653ff6b7d16e23
Crazydriver8 <krazydriver8@gmail.com>
2016-03-05 16:51:17 -0500

    Trying new networking model
    http://www.gamasutra.com/blogs/ChristianArellano/20150922/254218/UNET_Unity_5_Networking_Tutorial_Part_1_of_3__Introducing_the_HLAPI.php

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour {
+	// Information about the player
+	struct PlayerState {
+		public float x;
+		public float z;
+	}
+
+
+	/* Fields */
+
+	// Controls
+	string[] moveCommands = { "Horizontal", "Vertical" };
+	string[] combatCommands = { "Basic", "Strong", "Evade", "Grab", "Combo" };
+
+	// Information about the player, synchronized across all clients
+	[SyncVar]
+	private PlayerState state;
+
+
+	/* Instantiation through MonoBehaviour */
+
+	void Awake () {
+		InitState ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isLocalPlayer) {
+			foreach (string input in moveCommands) {
+				CmdMove (input);
+			}
+		}
+
+		SyncState ();
+	}
+
+
+	/* Helper methods for playing the game */
+
+	// Set the initial state of the player
+	[Server]
+	void InitState () {
+		state = new PlayerState {
+			x = 0.0f,
+			z = 0.0f
+		};
+	}
+
+	// Handles movement on the server
+	[Command]
+	void CmdMove(string movement)
+	{
+		float deltaX = 0.0f,
+			  deltaZ = 0.0f;
+
+		switch (movement) {
+			case "Horizontal":
+				deltaX = Input.GetAxis (movement);
+				break;
+
+			case "Vertical":
+				deltaZ = Input.GetAxis (movement);
+				break;
+
+			default:
+				break;
+		}
+
+		state = new PlayerState {
+			x = deltaX + state.x,
+			z = deltaZ + state.z
+		};
+	}
+
+	// Synchronizes state across clients
+	void SyncState()
+	{
+		transform.position = new Vector3 (state.x, 0, state.z);
+	}
+}
