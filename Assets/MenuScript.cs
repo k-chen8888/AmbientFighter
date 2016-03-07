6d94fc9035c2d8ca3d79c09ef3234e177d34e12e
Crazydriver8 <krazydriver8@gmail.com>
2016-03-07 15:46:32 -0500

    Added Menu

---
+++ Assets/MenuScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.F)) {
+			Application.LoadLevel ("Arena");
+		}
+		if (Input.GetKeyDown (KeyCode.S)) {
+			Application.LoadLevel ("SkillEditor");
+		}
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Application.Quit ();
+		}
+	}
+}
